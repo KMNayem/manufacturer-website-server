462fc4620e1acc53728366ff8dde2d5b85057b58
KMNayem <imna41695@gmail.com>
2022-05-27 12:05:13 +0600

    single product getting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,9 +24,18 @@ async function run(){
             const cursor = productCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+
+        app.get('/product/:id', async(req, res)=>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const product = await productCollection.findOne(query);
+          res.send(product);
+
         })
 
-        console.log('database connected')
+        
     }
     finally{
 
